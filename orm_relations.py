982d92fa25294bafd73a2d3a9c1f7dea5a9a9d09
igortereshchenko <tereshchenko.igor@gmail.com>
2020-11-05 15:48:44 +0200

    orm relations

--- orm_relations.py
+++ orm_relations.py
@@ -7,31 +7,56 @@ Base = declarative_base()
 
 class Human(Base):
     __tablename__ = "humans"
+    # TABLE COLUMNS
     # id
     id = Column("human_id", Integer, primary_key=True)
     # name
     name = Column(String(10))
     # age
     age = Column(Integer)
+    # END TABLE COLUMNS
 
-    def getName(self):
-        return "Dear, "+self.name
-    def nextYearAge(self):
-        return 1+self.age
+    # link to [Hobby1, Hobby2,... ]
+    hobbies = relationship("Hobby", back_populates="human")
+
+
 
 class Hobby(Base):
     __tablename__ = "hobby"
+
+    # TABLE COLUMNS
     # id
     id = Column(Integer, primary_key=True)
     # name
     name = Column(String(10))
-
+    # fk
     human_fk = Column(Integer, ForeignKey('humans.human_id'))
-    # link to class
-    human = relationship("Human", back_populates = "hobby")
+    # END TABLE COLUMNS
+
+    # link to class Human
+    human = relationship("Human", back_populates = "hobbies")
 
 
-Human.hobbies = relationship("Hobby", back_populates="humans")
 
-Base.metadata.create_all(engine)
+
+session = sessionmaker(engine)
+open_session = session()
+
+
+boban = Human(name="Boban", age=21)
+boban.hobbies = [ Hobby(name="drinking")]
+
+open_session.add(boban)
+open_session.commit()
+
+
+bob = open_session.query(Human).get(9)
+print(bob.name)
+for hobby in bob.hobbies:
+    print(hobby.name)
+
+hobbies = open_session.query(Hobby).filter(Hobby.name=='drinking')
+if hobbies:
+    for hobby in hobbies:
+        print(hobby.human.name)
 
